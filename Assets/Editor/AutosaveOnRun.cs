07072bb71e6c5ecc70733e3d9e5fb10dc859c2bb
Dana Wyss <=>
2016-09-30 11:50:13 -0600

    Add "AutoSave" script to save all scenes when playing in the Editor

---
+++ Assets/Editor/AutosaveOnRun.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+[InitializeOnLoad]
+public class AutosaveOnRun
+{
+    static AutosaveOnRun()
+    {
+        EditorApplication.playmodeStateChanged = () =>
+        {
+            if (EditorApplication.isPlayingOrWillChangePlaymode && !EditorApplication.isPlaying)
+            {
+                Debug.Log("Auto-Saving scenes before entering Play mode");
+
+                //EditorApplication.SaveScene();
+                for (var index = 0; index < UnityEngine.SceneManagement.SceneManager.sceneCount - 1; index++)
+                {
+                    EditorSceneManager.SaveScene(UnityEngine.SceneManagement.SceneManager.GetSceneAt(index));
+                }
+                EditorApplication.SaveAssets();
+            }
+        };
+    }
+}
