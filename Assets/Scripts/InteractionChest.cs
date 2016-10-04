d6e413b06feef22f3ea389ecffc5be5f32b946ef
Dana Wyss <=>
2016-10-04 14:20:40 -0600

    Rescale weapons when they are spawned (only the weapon holder transform) Set weapon layer to "Default" when spawned, as they only become weapons when they are equipped

--- Assets/Scripts/InteractionChest.cs
+++ Assets/Scripts/InteractionChest.cs
@@ -27,6 +27,17 @@ public class InteractionChest : Interactable
             //Instantiate(content, transform.position + new Vector3(0f, .2f, 0f), Quaternion.identity);
             // Random position within this transform
             var obj = (GameObject) Instantiate(content, Vector3.zero, Quaternion.identity);//Quaternion.Euler(270f, 0f, 0f));
+            if (obj.GetComponentInChildren<Weapon>())
+            {
+                // Weapons are scaled up by 5 to fit the avatar, so we need to reduce the scale to 1
+                obj.transform.localScale = Vector3.one;
+                foreach (var weapon in obj.GetComponentsInChildren<Weapon>())
+                {
+                    // We need to change the layer to "Default" as the layer "Weapon" is invisible to the Reticle (to avoid the weapon
+                    // flashing when it passes between the camera and the reticle
+                    weapon.gameObject.layer = LayerMask.NameToLayer("Default");
+                }
+            }
             var collider = obj.GetComponentInChildren<Collider>();
             if (collider != null)
             {
